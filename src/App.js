e0d97d5e8439a57b01ec20c57264a8e669a16579
Jasmine Francois <francois.jasminem@gmail.com>
2018-10-30 20:10:15 -0500

    Create json of movies, first pass at displaying info

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {movies} from './movies';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentMovie: {}
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                {movies.map((movie) =>
+                    <div>
+                        <p>{movie.title}</p>
+                        <p>{movie.rating}</p>
+                        <img
+                            alt={movie.title}
+                            src={movie.posterUrl}
+                            width={100}
+                        />
+                    </div>
+
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
