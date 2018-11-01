3262109bb9fdc21b94f1d2bf9c540e3917380136
Jasmine Francois <francois.jasminem@gmail.com>
2018-10-31 22:36:21 -0500

    Pull out function, add some basic styling with classNames

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
 import {movies} from './movies';
 
+import './styles.css';
+
 class App extends Component {
+    _setCurrentMovie = (movie) =>
+        this.setState({
+            currentMovie: movie
+        });
+
     constructor(props) {
         super(props);
+
+        this.setCurrentMovie = this._setCurrentMovie.bind(this);
 
         this.state = {
             currentMovie: null
@@ -13,21 +22,19 @@ class App extends Component {
     render() {
         return (
             <div>
-                <div>
+                <div className="movie-previews">
                     {movies.map((movie) =>
                         <button
-                            onClick={() =>
-                                this.setState({
-                                    currentMovie: movie
-                                })
-                            }
+                            className="preview-button"
+                            key={movie.title}
+                            onClick={() => this.setCurrentMovie(movie)}
                         >
                             <p>{movie.title}</p>
                             <p>{movie.releaseYear}</p>
                             <img
                                 alt={movie.title}
                                 src={movie.posterUrl}
-                                width={100}
+                                height={250}
                             />
                         </button>
                     )}
@@ -35,18 +42,23 @@ class App extends Component {
                 <div>
                     {
                         this.state.currentMovie ?
-                            <div>
-                                <p>{this.state.currentMovie.title}</p>
-                                <p>Rating: {this.state.currentMovie.rating}/10</p>
-                                <p>Description: {this.state.currentMovie.description}</p>
-                                <p>Director: {this.state.currentMovie.director}</p>
-                                <p>Release Year: {this.state.currentMovie.releaseYear}</p>
-                                <p>Categories: {this.state.currentMovie.categories}</p>
-                                <img
-                                    alt={this.state.currentMovie.title}
-                                    src={this.state.currentMovie.posterUrl}
-                                    width={300}
-                                />
+                            <div className="movie-information">
+                                <div>
+                                    <img
+                                        alt={this.state.currentMovie.title}
+                                        className="poster-image"
+                                        src={this.state.currentMovie.posterUrl}
+                                        width={350}
+                                    />
+                                </div>
+                                <div className="movie-text">
+                                    <p>{this.state.currentMovie.title}</p>
+                                    <p>Rating: {this.state.currentMovie.rating}/10</p>
+                                    <p>Description: {this.state.currentMovie.description}</p>
+                                    <p>Director: {this.state.currentMovie.director}</p>
+                                    <p>Release Year: {this.state.currentMovie.releaseYear}</p>
+                                    <p>Categories: {this.state.currentMovie.categories}</p>
+                                </div>
                             </div>
                             :
                             <div>
